f6de240164daf9c8f31c5bdcbcad44b268a4e8f6
watch1k <host.sf@gmail.com>
2016-05-08 14:08:08 +0300

    update

--- build/js/app.js
+++ build/js/app.js
@@ -123,5 +123,18 @@ $(document).ready(function(){
 			});
 		});
 	}());
+
+	// Google Map
+	function initialize() {
+		var mapProp = {
+			center:new google.maps.LatLng(51.508742,-0.120850),
+			zoom:15,
+			scrollwheel: false,
+			draggable: true,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		};
+		var map=new google.maps.Map(document.getElementById("map"),mapProp);
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 	
 });
